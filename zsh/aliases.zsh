374d4a610096da957994d54b632cd427cb7d40dc
Matej Janežič <janezic.mj@gmail.com>
2026-01-21 23:06:14 +0100

    feat(zsh): update aliases

--- zsh/aliases.zsh
+++ zsh/aliases.zsh
@@ -4,6 +4,10 @@ alias n=nvim_ve
 # alias vim=nvim_ve
 alias s="ssh-menu"
 
+# docker
+alias dp="docker ps --format 'table {{.Names}}\t{{.Ports}}'"
+alias dcp="docker compose ps --format 'table {{.Name}}\t{{.Ports}}'"
+
 # navigation
 alias cdgit="cd $GGROOT"
 alias icloud="cd $HOME/Library/Mobile\ Documents/com\~apple\~CloudDocs"
@@ -16,6 +20,7 @@ alias accentoff="defaults write -g ApplePressAndHoldEnabled -bool false"
 # git
 alias gros="git reset origin/$(git_current_branch) --soft"
 alias gcan="git commit -v --amend --no-edit"
+alias glogaa="git log --oneline --graph --all --pretty=format:\"%C(auto)%h %C(blue)(%aL/%cL)%C(auto)%(decorate) %s%Creset\""
 
 # unsorted
 alias ci="glab ci list"
@@ -24,7 +29,7 @@ alias grow="~/.local/bin/cbonsai -liWC -M 15 -t 2 -w 60 -s $(date +%s)"
 alias clean_ds_store='find . -name ".DS_Store" -type f -delete -print'
 
 # ruff
-alias ruffme="ruff format && ruff check --fix"
+alias ruffme="ruff check --fix && ruff format"
 
 # clipboard
 alias xcp="xclip -selection c"
