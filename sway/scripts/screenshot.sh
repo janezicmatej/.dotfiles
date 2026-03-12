e52005df827c7bd4f05322c265ac268c8e873cd8
Matej Janežič <janezic.mj@gmail.com>
2026-03-12 18:28:31 +0100

    feat(satty): replace flameshot

---
+++ sway/scripts/screenshot.sh
@@ -0,0 +1,5 @@
+#!/usr/bin/env bash
+
+# screenshot focused monitor with satty
+output=$(swaymsg -t get_outputs | jq -r '.[] | select(.focused) | .name')
+grim -o "$output" - | satty -f - --fullscreen
